6ab34eca58cbe9a06f42e5ad1430c0ce832ff644
Harish <harish96rs@gmail.com>
2022-09-06 09:55:05 +0530

    cros

--- src/global.js
+++ src/global.js
@@ -1,1 +1,2 @@
-export const API = "https://62dec963976ae7460be246b4.mockapi.io/";
+// export const API = "https://62dec963976ae7460be246b4.mockapi.io/";
+export const API = "https://harish-firsttry.herokuapp.com/";
