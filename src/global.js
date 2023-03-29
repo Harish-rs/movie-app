0d7a60264b8f2dd7acb60fbc6062529f9ad93d77
Harish <harish96rs@gmail.com>
2023-03-29 20:17:01 +0530

    commit refresh

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const API = "https://62dec963976ae7460be246b4.mockapi.io/";
-export const API = "https://harish-firsttry.herokuapp.com";
+export const API = "https://62dec963976ae7460be246b4.mockapi.io/";
+// export const API = "https://harish-firsttry.herokuapp.com";
